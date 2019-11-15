319deb5468b191e141eac626ff1661d54582de86
amishani <amishani@gmail.com>
2019-11-15 16:00:55 +0800

    last

--- webnode/main/views.py
+++ webnode/main/views.py
@@ -5,7 +5,7 @@ def main(request):
     '''
     Render the main page
     '''
-    context = {'like':'Django 很棒'}
+    context = {'like':'我們的服務技術,協助您快速架設網站'}
     return render(request, 'main/main.html', context)
 
 def about(request):
